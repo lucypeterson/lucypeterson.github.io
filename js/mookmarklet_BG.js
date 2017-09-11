86c6243a8d243222482f8321b7cac3ecf181897e
lucypeterson <luciana.peterson@cca.edu>
2017-09-11 10:44:13 -0700

    initial background change to red

--- js/mookmarklet_BG.js
+++ js/mookmarklet_BG.js
@@ -32,7 +32,7 @@ function initMyBookmarklet(){
 
         //YOUR CODE GOES HERE!
 
-        javascript:void(document.body.style.backgroundColor = "red";
+        javascript:void(document.body.style.background = "red";
 Try it Yourself »
 )
 
