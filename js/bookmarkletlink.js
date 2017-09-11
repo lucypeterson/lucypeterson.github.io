0bb307b12b84b409c1d8f95cfc48e7e68b57ab79
lucypeterson <luciana.peterson@cca.edu>
2017-09-11 10:29:55 -0700

    js folder

---
+++ js/bookmarkletlink.js
@@ -0,0 +1,6 @@
+javascript:(function(){
+    document.body.appendChild(document.createElement('script')).src='Your Link Goes Here';
+})();
+javascript:(function(){
+    document.body.appendChild(document.createElement('script')).src='Your Link Goes Here';
+})();
