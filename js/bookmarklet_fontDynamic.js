b9f9baf6aa7c64cfc6e2807a8354e1e248fc3d7e
lucypeterson <luciana.peterson@cca.edu>
2017-09-13 09:02:51 -0700

    mouse move

--- js/bookmarklet_fontDynamic.js
+++ js/bookmarklet_fontDynamic.js
@@ -32,14 +32,14 @@ function initMyBookmarklet(){
 
         //YOUR CODE GOES HERE!
 
-$(document).click(function({ 
-    var p=document.getElementsByTagName('*');
+$(document).mousemove(function(}{
+ var p=document.getElementsByTagName('*');
         for(i=0;i<p.length;i++){
             if(p[i].style.fontSize){
                 var s=parseInt(p[i].style.fontSize.replace("px",""));
             } else {
                 var s=12;}s+=2;p[i].style.fontSize=s+"px"}
-            }):
+}):
        
 
 
