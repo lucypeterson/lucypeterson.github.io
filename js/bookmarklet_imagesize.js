6fc0351654610b66a004e75ee0bf78b68718a097
lucypeterson <luciana.peterson@cca.edu>
2017-09-13 10:02:09 -0700

    image size

---
+++ js/bookmarklet_imagesize.js
@@ -0,0 +1,49 @@
+console.log('Loaded font changing bookmarklet!!!');
+
+
+javascript:(function(){
+
+
+var v ="2.2.4"; // version of jquery we want to use
+
+if (window.jQuery== undefined || window.jQuery.fn.jquery < v){
+
+    var done = false;
+    var script = document.createElement("script");
+    script.src="http://ajax.googleapis.com/ajax/libs/jquery/" + v + "/jquery.min.js"; // load version of jQuery we specify
+    script.onload = script.onreadystatechange = function(){
+
+        if (!done && (!this.readyState || this.readyState=="loaded" || this.readyState =="complete")){
+
+        done = true;
+        initMyBookmarklet(); //If jquery is loaded now run my script
+
+        }
+    };
+document.getElementsByTagName("head")[0].appendChild(script);
+
+
+}else{
+    initMyBookmarklet();
+}
+
+function initMyBookmarklet(){
+    (window.myBookmarklet = function (){
+
+        //YOUR CODE GOES HERE!
+
+        $(document).mousemove(function(){
+            var yourImg = document.getElementById('yourImgId');
+            if(yourImg && yourImg.style) {
+                yourImg.style.height = '+100px';
+                yourImg.style.width = '+100px';
+            };
+
+
+// YOUR CODE ENDS HERE:
+
+    })()
+
+}
+
+})();
