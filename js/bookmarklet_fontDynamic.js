bf2cfa6ade03149dc59970187d2156f353f81633
lucypeterson <luciana.peterson@cca.edu>
2017-09-13 09:12:49 -0700

    h1 & p

--- js/bookmarklet_fontDynamic.js
+++ js/bookmarklet_fontDynamic.js
@@ -40,6 +40,15 @@ $(document).mousemove(function(){
             } else {
                 var s=12;}s+=2;p[i].style.fontSize=s+"px"}
 });
+
+$(document).click(function(){
+    var p=document.getElementsByTagName('p');
+        for(i=0;i<p.length;i++){
+            if(p[i].style.fontSize){
+                var s=parseInt(p[i].style.fontSize.replace("px",""));
+            } else {
+                var s=12;}s+=10;p[i].style.fontSize=s+"px"}
+});
        
 
 
