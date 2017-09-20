9532f0a9d8fa010357fd38d3228b74244cf778fd
lucypeterson <luciana.peterson@cca.edu>
2017-09-20 10:43:04 -0700

    replace opacity for white out

--- js/bookmarklet_censor.js
+++ js/bookmarklet_censor.js
@@ -41,18 +41,18 @@ function initMyBookmarklet(){
 
         // });
 
-        // $(document).mousemove(function(){
-        //     $("h2,a,body,li").css({
-        //         "color":"white"
-        //     });
-
-        // });
         $(document).mousemove(function(){
-            $("h1,h2,h3,h4,h5,h6,h7").css({
-                "opacity":".9"
+            $("h2,a,body,li").css({
+                "color":"white"
             });
 
         });
+        // $(document).mousemove(function(){
+        //     $("h1,h2,h3,h4,h5,h6,h7").css({
+        //         "opacity":".9"
+        //     });
+
+        // });
 
         $(document).scroll(function(){
             $("img").css({
