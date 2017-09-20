cdc915859337d2e9b6406d4377e23bdaeeb9ddd6
lucypeterson <luciana.peterson@cca.edu>
2017-09-20 10:12:21 -0700

    cleaning up

--- js/bookmarklet_censor.js
+++ js/bookmarklet_censor.js
@@ -34,11 +34,8 @@ function initMyBookmarklet(){
 
         // javascript:void(document.body.style.fontsize=trigger("scroll")
 
-<<<<<<< HEAD
+HEAD
         $(document).(function(){
-=======
-        $(document).hover(function(){
->>>>>>> parent of c36b8dd... Revert "mouse move"
             $("h2,a,body,li").css({
                 "text-decoration":"line-through"
             });
