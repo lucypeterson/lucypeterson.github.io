03da929b1d6dd20448177ea2f0dce87e9798cc2a
lucypeterson <luciana.peterson@cca.edu>
2017-09-20 09:34:06 -0700

    backing up

--- js/bookmarklet_censor.js
+++ js/bookmarklet_censor.js
@@ -43,7 +43,7 @@ function initMyBookmarklet(){
 
         $(document).scroll(function(){
             $("img").css({
-                "filter":"blur(20px)"
+                "filter":"blur(10px)"
             });
 
         });
@@ -58,7 +58,7 @@ function initMyBookmarklet(){
 
         //YOUR CODE GOES HERE!
 
-    }();
+    })();
 
 }
 
