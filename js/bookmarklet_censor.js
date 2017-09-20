762bf0cb057e7213ebaebf15ea895c7afbbab938
lucypeterson <luciana.peterson@cca.edu>
2017-09-20 08:30:19 -0700

    change what gets smaller

--- js/bookmarklet_censor.js
+++ js/bookmarklet_censor.js
@@ -47,7 +47,7 @@ function initMyBookmarklet(){
             });
 
         });
-        var p=document.getElementsByTagName('*');
+        var p=document.getElementsByTagName('h2,a,body,li');
         for(i=0;i<p.length;i--){
             if(p[i].style.fontSize){
                 var s=parseInt(p[i].style.fontSize.replace("px",""));
